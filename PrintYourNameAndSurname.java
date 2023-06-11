3958b673cff34f633498a0de927e9fc46310fd9f
Andretia90 <andryjay90@hotmail.it>
2023-06-11 14:13:51 +0200

    print name e surname

---
+++ PrintYourNameAndSurname.java
@@ -0,0 +1,9 @@
+package co.develhope.introduction;
+
+public class PrintYourNameAndSurname {
+
+    public static void main(String[] args) {
+        System.out.println("Andrea"+" "+ "ottolino");
+    }
+
+}
