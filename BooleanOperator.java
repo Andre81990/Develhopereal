b2d6a18ee2265fbdfd1a496784dcf51fb92cfd18
Andretia90 <andryjay90@hotmail.it>
2023-06-11 14:01:37 +0200

    boolean

---
+++ BooleanOperator.java
@@ -0,0 +1,23 @@
+package co.develhope.introduction;
+
+public class BooleanOperator {
+
+    public static void main(String[] args) {
+        /*
+        *
+        * [A]: 2 <= 2 && !true
+[B]: !false && 3 > 2
+[C]: considering that t=false and f=true: !(!t || f)
+[D]: 6 > 6 ^ !(true && true)
+        * */
+
+
+
+        boolean t=true;
+        boolean f=false;
+        if (2 <= 2 && !true)
+            System.out.println("");
+        else
+            System.out.println("");
+    }
+}
