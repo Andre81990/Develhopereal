96493cdce307b0bbc6994fec733479d0e4725056
Andretia90 <andryjay90@hotmail.it>
2023-06-11 14:14:12 +0200

    start

---
+++ Start.java
@@ -0,0 +1,10 @@
+package co.develhope.introduction;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello develhope java student, my  birthday is 16 july");
+    }
+}
