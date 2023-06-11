ec8bf1f311de405423809cf9bb5c14c80a7e2747
Andretia90 <andryjay90@hotmail.it>
2023-06-11 14:02:24 +0200

    ifelse

---
+++ Ifelse.java
@@ -0,0 +1,16 @@
+package co.develhope.introduction;
+
+public class Ifelse
+
+{
+
+    public static void main(String[] args) {
+
+
+        String myname = "anrea";
+
+        if (myname.length()%2==0)
+        {System.out.println("e pari");}
+else {
+System.out.println("e dispari");}
+}}
