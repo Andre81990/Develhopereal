ae05697a5d4a3fed5d150a1c664c813dcb4ee3f0
Andretia90 <andryjay90@hotmail.it>
2023-06-11 14:15:37 +0200

    stringhandling

---
+++ Stringhandling.java
@@ -0,0 +1,15 @@
+package co.develhope.introduction;
+
+public class Stringhandling
+{
+
+    public static void main(String[] args) {
+
+
+        String st1 = "ciao";
+        String st2 = "come va?";
+//char array non è servito ma potevo anche fare charArray =st1.substring(0,2)+st2.substring(6,8)
+        //Syso charArray
+        System.out.println(st1.substring(0,2)+st2.substring(6,8));
+    }
+}
