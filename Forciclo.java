8865ad381f32a58b86cf7cd14744be3472c4f2b7
Andre81990 <38924736+Andre81990@users.noreply.github.com>
2023-06-13 16:00:17 +0200

    Add files via upload

---
+++ Forciclo.java
@@ -0,0 +1,22 @@
+package co.develhope.introduction;
+
+public class Forciclo
+
+{
+
+    public static void main(String[] args) {
+
+
+//5 e 25
+        //int min = 5;
+        //int max = 25;
+
+
+        for (int i=0;i<=25;i++)
+        {
+            System.out.println(i);
+
+        }
+    }
+
+}
