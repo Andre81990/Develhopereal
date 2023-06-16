df836626fae6770413639fe6935707041b0ee2cc
Andre81990 <38924736+Andre81990@users.noreply.github.com>
2023-06-16 18:00:43 +0200

    Add files via upload

--- Arraysvero.java
+++ Arraysvero.java
@@ -2,52 +2,55 @@ package co.develhope.introduction;
 
 public class Arraysvero
 {
+    public static void main(String[] args) {
+
+        String[] cars = {"ferrari", "audi", "fiat", "lambo", "merced"};
+
+        System.out.println("la lunghezza dell array è " + cars.length);
+
+        //parte da zero e non da uno l indice
+        System.out.println("il terzo elemento è " + cars[2]);
+
+//vecchio array
+        String[] carse = {"ferrari", "audi", "fiat", "lambo", "merced"};
 
 
-
-
-        public static void main(String[] args) {
-
-        String[]  cars={"ferrari" , "audi","fiat","lambo","merced"};
-
-        System.out.println(cars.length);
-        System.out.println(cars[2]);
-
-
-
-
-
-        String[]  carse={"ferrari" , "audi","fiat","lambo","merced"};
-
-
-
-
+        //nuovo array
         String[] copyDentro = new String[5];
 
 
         System.arraycopy(carse, 0, copyDentro, 0, 5);
 
-        for (String car:copyDentro
-        ) {
+        for (String car : copyDentro) {
+            {
+                System.out.println(car + " è un elem della lista ");
 
-            System.out.println(car +" ");
+            }
 
         }
-/*
-        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
+
+        int[] primeNumbers;
+        //={2,3,5,7,11,13};
 
 
+        primeNumbers = new int[6];
 
-        String[] copyTo = new String[7];
-        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
-        for (String coffee : copyTo) {
-            System.out.print(coffee + " ");
-        }
-        * */
+        primeNumbers[0]=2;
+        primeNumbers[1]=3;
+        primeNumbers[2]=5;
+        primeNumbers[3]=7;
+        primeNumbers[4]=11;
+        primeNumbers[5]=13;
 
+for (int i = 0; i<primeNumbers.length; i++)
+{
+    System.out.println(primeNumbers[i]);
+    
+}
 
     }
-    }
+}
+
 
 
 
